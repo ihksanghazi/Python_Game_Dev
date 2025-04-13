60b58ff1946a9efa13ebe7b2fdd480a62a51fe74
naskhi <nursandyihksan97@gmail.com>
2025-04-13 12:03:57 +0700

    Pertemuan 5

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ FPS = 32
 frame_size_x = 289
 frame_size_y = 511
 window_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
+high_score = 0
 game_sprites = {}
 game_sounds = {}
 player = 'gallery/sprites/astro.png'
@@ -21,6 +22,7 @@ pygame.display.set_caption('Astro Man')
 def main_game():
     player_x = int(frame_size_x/5)
     player_y = int(frame_size_x/2)
+    global high_score
     base_x = 0
     player_jump = False
     player_jump_acc = -8
@@ -28,6 +30,7 @@ def main_game():
     player_max_vel_y = 10
     player_acc_y = 1
     pipe_vel_x = -4
+    score = 0
     new_pipe_1 = get_random_pipe()
     new_pipe_2 = get_random_pipe()
     upper_pipes = [
@@ -49,6 +52,20 @@ def main_game():
                     player_vel_y = player_jump_acc
                     player_jump = True
                     game_sounds['jump'].play()
+
+        crash_test= is_collide(player_x, player_y, upper_pipes, lower_pipes)
+        if crash_test:
+            if score > high_score :
+                high_score = score
+            return score
+
+        player_mid_pos = player_x + game_sprites['player'].get_width()/2
+        for pipe in upper_pipes:
+            pipe_mid_pos = pipe['x'] + game_sprites['pipe'][0].get_width()/2
+            if pipe_mid_pos<= player_mid_pos < pipe_mid_pos +4:
+                score +=1
+                print(f"Your score is {score}")
+                game_sounds['point'].play()
         if player_vel_y < player_max_vel_y and not player_jump:
             player_vel_y += player_acc_y
         if player_jump:
@@ -72,9 +89,39 @@ def main_game():
         if upper_pipes[0]['x'] < -game_sprites['pipe'][0].get_width():
             upper_pipes.pop(0)
             lower_pipes.pop(0)
-            
+
+        score_font = pygame.font.SysFont('Impact', 32)
+        score_surface = score_font.render(str(score), True,(255,255,255))
+        score_rect = score_surface.get_rect()
+        score_rect.midtop = (frame_size_x/2, 32)
+        window_screen.blit(score_surface, score_rect)
+
         pygame.display.update()
         fps_controller.tick(FPS)
+
+def game_over_screen(score):
+    global high_score
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN and (event.key == K_RETURN or event.key == K_UP):
+                return
+
+            else:
+                window_screen.blit(game_sprites['background'], (0, 0))
+                text_font = pygame.font.SysFont('Impact', 24)
+                game_over_surface = text_font.render("Game Over !", True, (255,255,255))
+                score_surface = text_font.render("Score : " + str(score), True, (255,255,255))
+                continue_surface = text_font.render("Press 'ENTER' To Continue !", True, (255,255,255))
+                high_score_surface = text_font.render("High Score : " + str(high_score), True, (255,255,255))
+                window_screen.blit(game_over_surface, (80, 100))
+                window_screen.blit(score_surface, (100, 170))
+                window_screen.blit(continue_surface, (20, 230))
+                window_screen.blit(high_score_surface, (80, 430))
+                pygame.display.update()
+                fps_controller.tick(FPS)
 
 def welcome_screen():
     player_x = int(frame_size_x/5)
@@ -99,6 +146,24 @@ def welcome_screen():
                 pygame.display.update()
                 fps_controller.tick(FPS)
 
+def is_collide(player_x, player_y, upper_pipes, lower_pipes):
+    if player_y > frame_size_y * 0.7 or player_y <0: 
+        game_sounds['hit'].play()
+        return True #conditon if the player collide with ground
+
+    for pipe in upper_pipes:
+        pipe_height = game_sprites['pipe'][0].get_height()
+        if(player_y < pipe_height + pipe['y'] and abs(player_x - pipe['x']) < game_sprites['pipe'] [0].get_width()):
+            game_sounds['hit'].play()
+            return True
+
+    for pipe in lower_pipes:
+        if (player_y + game_sprites['player'].get_height() > pipe['y']) and abs(player_x - pipe['x']) < game_sprites['pipe'][0].get_width():
+            game_sounds['hit'].play()
+            return True
+
+    return False
+
 def get_random_pipe():
     pipe_height = game_sprites['pipe'][0].get_height() #get the height of the pipe sprite
     offset = frame_size_y/3
@@ -122,4 +187,5 @@ game_sounds['jump'] = pygame.mixer.Sound('gallery/audio/jump.wav')
 
 while True:
     welcome_screen()
-    main_game()
+    score = main_game()
+    game_over_screen(score)
