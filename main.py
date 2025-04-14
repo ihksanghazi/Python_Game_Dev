c1d00802c9f3924cc8b9747586f61ca892cc7c5c
naskhi <nursandyihksan97@gmail.com>
2025-04-14 12:36:01 +0700

    pertemuan 7

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ frame_size_x = 900
 frame_size_y = 500
 
 FPS = 60            #Kecepatan game (60 frame per detik)
+velocity = 5
 ship_width = 55     #Lebar pesawat (55 pixel)
 ship_height = 40    #Tinggi pesawat (40 pixel)
 max_num_of_bullet = 5  #Maksimal peluru yang bisa ditembak sekaligus
+bullet_velocity = 7
 
 window_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
 
@@ -21,15 +23,64 @@ black = (0, 0, 0)  # RGB Code for Black
 green = (110, 194, 54)  # RGB Code for Green Bullet
 blue = (23, 54, 235)  # RGB Code for Blue Bullet
 
+border = pygame.Rect((frame_size_x//2)-5,0,10,frame_size_y)
+
 background = pygame.transform.scale(pygame.image.load('gallery/sprites/background.png'),(frame_size_x, frame_size_y)).convert()
 space_shooter_logo = pygame.image.load('gallery/sprites/space_shooter.png').convert_alpha()
 space_shooter_logo = pygame.transform.scale(space_shooter_logo, (300, 150))
-green_ship_img = pygame.transform.rotate(pygame.image.load('gallery/sprites/green_ship.png').convert_alpha(), 270)
-blue_ship_img = pygame.transform.rotate(pygame.image.load('gallery/sprites/blue_ship.png').convert_alpha(), 90)
+green_ship_img = pygame.transform.rotate(pygame.image.load('gallery/sprites/shipGreen.png').convert_alpha(), 270)
+blue_ship_img = pygame.transform.rotate(pygame.image.load('gallery/sprites/shipBlue.png').convert_alpha(), 90)
 green_ship = pygame.transform.scale(green_ship_img,(ship_width,ship_height)).convert_alpha()
 blue_ship = pygame.transform.scale(blue_ship_img,(ship_width,ship_height)).convert_alpha()
 
 bullet_fire_sound = pygame.mixer.Sound('gallery/audio/sfx_fire.ogg')
+
+def blue_movement_handler(keys_pressed, blue):
+    if keys_pressed[pygame.K_LEFT] and blue.x - velocity > border.x + border.width - 5:  #Left
+        blue.x -= velocity
+    if keys_pressed[pygame.K_RIGHT] and blue.x - velocity + blue.width < frame_size_x - 5:  #Right
+        blue.x += velocity
+    if keys_pressed[pygame.K_UP] and blue.y - velocity > 0:  #Up
+        blue.y -= velocity
+    if keys_pressed[pygame.K_DOWN] and blue.y - velocity + blue.height < frame_size_y - 5:  #Down
+        blue.y += velocity
+
+def green_movement_handler(key_pressed, green):
+    if key_pressed[pygame.K_w] and green.y - velocity > 0:
+        green.y -= velocity
+    if key_pressed[pygame.K_a] and green.x - velocity > 5:
+        green.x -= velocity
+    if key_pressed[pygame.K_s] and green.y - velocity + green.height < frame_size_y - 5:
+        green.y += velocity
+    if key_pressed[pygame.K_d] and green.x - velocity +green.width < border.x -5:
+        green.x += velocity
+
+def handle_bullets(green_bullets, blue_bullets, green,blue):
+    for bullet in green_bullets:
+        bullet.x += bullet_velocity
+        if blue.colliderect(bullet):
+            green_bullets.remove(bullet)
+        elif bullet.x > frame_size_x:
+            green_bullets.remove(bullet)
+    
+    for bullet in blue_bullets:
+        bullet.x -= bullet_velocity
+        if green.colliderect(bullet):
+            blue_bullets.remove(bullet)
+        elif bullet.x < 0:
+            blue_bullets.remove(bullet)
+
+def draw_window(green_rect, blue_rect, green_bullets, blue_bullets):
+    window_screen.blit(background, (0, 0))
+    pygame.draw.rect(window_screen,black,border)
+    window_screen.blit(green_ship,(green_rect.x,green_rect.y))
+    window_screen.blit(blue_ship,(blue_rect.x,blue_rect.y))
+    for bullet in green_bullets:
+        pygame.draw.rect(window_screen,green,bullet)
+    for bullet in blue_bullets:
+        pygame.draw.rect(window_screen,blue,bullet)
+
+    pygame.display.update()    
 
 def main():
     clock = pygame.time.Clock()
@@ -45,14 +96,21 @@ def main():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(green_bullets) < max_num_of_bullet:
+                    bullet = pygame.Rect(green_rect.x+green_rect.width,green_rect.y+green_rect.height//2,10,5)
+                    green_bullets.append(bullet)
                     bullet_fire_sound.play()
                 if event.key == pygame.K_RCTRL and len(blue_bullets) < max_num_of_bullet:
+                    bullet = pygame.Rect(blue_rect.x,blue_rect.y+blue_rect.height//2,10,5)
+                    blue_bullets.append(bullet)
                     bullet_fire_sound.play()
 
-        window_screen.blit(background, (0, 0))
-        window_screen.blit(green_ship,(green_rect.x,green_rect.y))
-        window_screen.blit(blue_ship,(blue_rect.x,blue_rect.y))
-        pygame.display.update()
+        # print(green_bullets, blue_bullets)
+        keys_pressed = pygame.key.get_pressed()
+        # print(keys_pressed[pygame.K_LEFT],keys_pressed[pygame.K_RIGHT])
+        green_movement_handler(keys_pressed, green_rect)
+        blue_movement_handler(keys_pressed, blue_rect)
+        handle_bullets(green_bullets,blue_bullets,green_rect,blue_rect)
+        draw_window(green_rect, blue_rect, green_bullets, blue_bullets)
 
 def welcome_screen():
     while True:
