b0d4bc205ca78990c3c3a0ffd274ee60c73ec9b8
naskhi <nursandyihksan97@gmail.com>
2025-04-17 07:25:58 +0700

    Pertemuan 10

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 pygame.init()
 frame_size_x = 800 
@@ -26,16 +27,50 @@ player_jump = pygame.image.load("gallery/sprites/player/Player3.png").convert_al
 player_rect = player.get_rect(midbottom=(80, 300))
 player_gravity = 0
 
+jump_sound = pygame.mixer.Sound('gallery/audio/jump.mp3')
+
+back_sound = pygame.mixer.Sound('gallery/audio/backsound.mp3')
+back_sound.play(loops=-1)
+back_sound.set_volume(0.5)
+
 skybox = pygame.image.load('gallery/sprites/Sky.png').convert()
 ground = pygame.image.load('gallery/sprites/Ground.png').convert()
 
+enemy_frame1 = pygame.image.load("gallery/sprites/enemies/Enemy.png").convert_alpha()
+enemy_frame2 = pygame.image.load("gallery/sprites/enemies/Enemy_2.png").convert_alpha()
+enemy_frames = [enemy_frame1, enemy_frame2]
+enemy_frame_index = 0
+enemy = enemy_frames[enemy_frame_index]
+
+enemy2_frame1 = pygame.image.load("gallery/sprites/enemies/Enemy2.png").convert_alpha()
+enemy2_frame2 = pygame.image.load("gallery/sprites/enemies/Enemy2_2.png").convert_alpha()
+enemy2_frames = [enemy2_frame1, enemy2_frame2]
+enemy2_frame_index = 0
+enemy2 = enemy2_frames[enemy2_frame_index]
+
+obstacle_rect_list = []
+
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1000)
+
+enemy_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(enemy_animation_timer, 200)
+
+enemy2_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(enemy2_animation_timer, 500)
+
 def active_game():
-    global player_gravity 
+    global player_gravity, obstacle_rect_list 
     window_screen.blit(skybox, (0,0))    #Tampilkan background
     window_screen.blit(ground, (0, 320))    #Tampilkan tanah/ground
     score = display_score()
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 320:
+        player_rect.bottom = 320
     player_animation()   #Jalankan animasi pemain
     window_screen.blit(player, player_rect)   #Tampilkan pemain di layar
+    obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
 
 def inactive_game():
@@ -57,12 +92,52 @@ def display_score():
     window_screen.blit(score, score_rect)
     return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+            if obstacle_rect.bottom == 320:
+                window_screen.blit(enemy, obstacle_rect)
+            else:
+                window_screen.blit(enemy2, obstacle_rect)
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    else:
+        return []
+
+def spawn_enemy():
+    global enemy_frame_index, enemy2_frame_index, enemy, enemy2
+    if event.type == obstacle_timer:
+        if randint(0, 2):
+            print("enemy has been spawned")
+            obstacle_rect_list.append(enemy.get_rect(bottomright = (randint(900, 1100), 320)))
+        else:
+            obstacle_rect_list.append(enemy2.get_rect(bottomright = (randint(900, 1100), 210)))
+    if event.type == enemy_animation_timer:
+        if enemy_frame_index == 0:
+            enemy_frame_index = 1
+        else:
+            enemy_frame_index = 0
+        enemy = enemy_frames[enemy_frame_index]
+
+    if event.type == enemy2_animation_timer:
+        if enemy2_frame_index == 0:
+            enemy2_frame_index = 1
+        else:
+            enemy2_frame_index = 0
+        enemy2 = enemy2_frames[enemy2_frame_index]
+
 def player_animation():
     global player_index, player
     player_index += 0.1
-    if player_index >= len(player_walk):
-        player_index = 0
-    player = player_walk[int(player_index)]
+    if player_rect.bottom < 320:
+        player = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk):
+            player_index = 0
+        player = player_walk[int(player_index)]
+
 
 while True:
     for event in pygame.event.get():
@@ -71,6 +146,11 @@ while True:
             exit()
 
         if game_active:
+            spawn_enemy()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == 320:
+                    jump_sound.play()
+                    player_gravity = -20
             print("Game Active")
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
